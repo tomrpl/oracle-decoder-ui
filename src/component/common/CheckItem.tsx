a469c7e84308476947ce9812d4ce0c5c3761501e
Reppelin Tom <tomireppelin@gmail.com>
2024-07-25 14:02:24 +0200

    feat(typo)

--- src/component/common/CheckItem.tsx
+++ src/component/common/CheckItem.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { BiCheckDouble, BiError, BiCaretDown, BiCaretUp } from "react-icons/bi";
-import { ErrorTypes, ErrorMessages } from "../../services/errorTypes";
 
 interface CheckItemProps {
   title: string;
